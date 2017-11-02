02384040cc010b3bf6f0186f0ddfccf792a7995b
Mateusz Kulesza <eduwebgit@gmail.com>
2017-11-02 15:51:43 +0100

    2.2. Atrybuty komponentu - finished

--- src/app/ui/static-card.component.ts
+++ src/app/ui/static-card.component.ts
@@ -3,7 +3,10 @@ import { Attribute, Component, OnInit } from '@angular/core'
 @Component({
   selector: 'static-card',
   template: `
-  <div class="card">
+  <div class="card" [ngClass]="{
+    'bg-info text-white': type == 'message',
+    'bg-danger text-white': type == 'error'
+  }">
     <div class="card-body">
       {{ bodyText }}
     </div>
@@ -13,9 +16,9 @@ import { Attribute, Component, OnInit } from '@angular/core'
 })
 export class StaticCardComponent implements OnInit {
 
-  constructor(@Attribute('text') public bodyText) {
-    
-  }
+  constructor(
+    @Attribute('text') public bodyText,
+    @Attribute('type') public type) { }
 
   ngOnInit() {
   }
