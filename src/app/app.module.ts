8dd5515e187f02e40ac8a930f1b13121dd2412a4
Mateusz Kulesza <eduwebgit@gmail.com>
2017-11-02 15:00:03 +0100

    1.1. Statyczny komponent - finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StaticCardComponent } from './ui/static-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StaticCardComponent
   ],
   imports: [
     BrowserModule
