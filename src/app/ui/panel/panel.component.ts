afaa42439c7e92acca9ab9d059e537257c674833
Mateusz Kulesza <eduwebgit@gmail.com>
2017-11-04 16:28:18 +0100

    3.1. Projekcja z ngContent - start

---
+++ src/app/ui/panel/panel.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'panel',
+  template: `
+    <p>
+      panel works!
+    </p>
+  `,
+  styles: []
+})
+export class PanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
