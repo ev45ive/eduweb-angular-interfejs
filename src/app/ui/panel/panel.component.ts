3b7148cd3121878e730765c2c8a66c86a0e8b977
Mateusz Kulesza <eduwebgit@gmail.com>
2017-11-04 16:48:04 +0100

    3.2. MultiProjekcja z select - start

--- src/app/ui/panel/panel.component.ts
+++ src/app/ui/panel/panel.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
   selector: 'panel',
   template: `
     <div class="card">
+      <div class="card-header">
+        <h5>{{title}}</h5>
+      </div>
       <div class="card-body">
         <ng-content></ng-content>
+      </div>
+      <div class="card-footer">
       </div>
     </div>
   `,
   styles: []
 })
 export class PanelComponent implements OnInit {
+
+  @Input()
+  title
 
   constructor() { }
 
