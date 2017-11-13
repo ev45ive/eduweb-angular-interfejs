88fa35794f8eab57e4a0a78a20065321b137e8cd
Mateusz Kulesza <eduwebgit@gmail.com>
2017-11-13 14:19:08 +0100

    4.1. Własne dyrektywy - start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  menu = {
+    items:[
+      { label: 'Pierwsza opcja' },
+      { label: 'Druga opcja' },
+      { label: 'Jeszcze jedna...' },
+    ]
+  }
+
+  addMenuItem(label){
+    this.menu.items.push({
+      label
+    })
+  }
 }
