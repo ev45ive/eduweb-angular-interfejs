8902bc9ba133fecd3c36cf0ddbde829b4b3ea53a
Mateusz Kulesza <eduwebgit@gmail.com>
2017-11-13 15:22:29 +0100

    4.1. Własne dyrektywy i ElementRef - finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DropdownDirective } from './ui/dropdown/dropdown.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule
