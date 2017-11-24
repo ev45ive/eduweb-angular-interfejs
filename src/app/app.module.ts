fb00ad9073115d2520c8354c6064bfd0dff91385
Mateusz Kulesza <eduwebgit@gmail.com>
2017-11-24 12:58:43 +0100

    5.1. Podmiana szablonu z NgIfElse - start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostsFeedComponent } from './social/posts-feed.component';
+import { PostActionsComponent } from './social/post-actions.component';
+import { PostHeaderComponent } from './social/post-header.component';
+import { NoResultsComponent } from './social/no-results.component';
+import { PostsFilterComponent } from './social/posts-filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsFeedComponent,
+    PostActionsComponent,
+    PostHeaderComponent,
+    NoResultsComponent,
+    PostsFilterComponent
   ],
   imports: [
     BrowserModule
